b005b45e78efb966e7d1d878843899f2674dfdd2
Kairat Mendybaev <113317646+kairat-mendybaev@users.noreply.github.com>
2025-03-14 00:02:28 -0700

    ContentB.js

---
+++ ContentB.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from './SharedComponents';
+
+export function ContentB() {
+  return (
+    <div>
+      <h2>Content B</h2>
+      <Button text="Click B" />
+    </div>
+  );
+}
